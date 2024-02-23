02bdcd8b1ca7335684f888d1c33bf1ac1d659b03
Aman Yadav <84045140+amanyadav91@users.noreply.github.com>
2024-02-23 12:51:43 +0530

    Order Success Page

--- Success-page/src/components/ThankYou.jsx
+++ Success-page/src/components/ThankYou.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './ThankYou.css';
 function ThankYou() {
     return (
-        <div>
+        <div className='thankpage'>
             <div className="thank-you">
+            <img width="188" height="188" src="https://img.icons8.com/3d-fluency/188/ok.png" alt="ok"/>
                 <h1>Thank you for ordering!</h1>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
                     sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliqua.</p>
